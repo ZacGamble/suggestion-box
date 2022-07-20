8d7f1eaa2721551c2ed3ff903aac73be2ef0ff25
gagelasher <rsgage21@gmail.com>
2022-07-20 09:31:45 -0600

    new changes

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+include 'connection.php';
+?>
+
 <html lang="en">
 
 <head>
@@ -9,61 +13,54 @@
 </head>
 
 <body>
-    <header>
+    <header class="d-flex align-items-center justify-content-center">
         <img src="https://images.unsplash.com/photo-1516414447565-b14be0adf13e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFuJTIwd3JpdGluZyUyMG5vdGV8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" class="header-img">
     </header>
     <main>
-        <div class="p-5">
+        <div class="p-5 d-flex align-items-center justify-content-center">
 
-            BINGO BONGO
             <?php
 
 
-            if (isset($_POST['submit'])) {
-                $firstName = $_POST['name'];
-                $email = $_POST['email'];
-                $suggestion = $_POST['suggestion'];
-            }
-            $servername = "localhost";
-            $username = "username";
-            $password = "password";
-            $dbname = "suggestions";
-
-            // Create connection hello worlds
-            $conn = new mysqli($servername, $username, $password, $dbname);
-
-            // Check connection
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            }
-            echo "Connected successfully";
-
-            $stmt = $conn->prepare("INSERT INTO suggestions (name, email, suggestion) VALUES (?, ?, ?)");
-            $stmt->bind_param(
-                "sss",
-                htmlspecialchars($firstName, ENT_QUOTES),
-                htmlspecialchars($email, ENT_QUOTES),
-                htmlspecialchars($suggestion, ENT_QUOTES)
-            );
 
             if (isset($_POST['submit'])) {
-                $stmt->execute();
+                $firstName = htmlspecialchars($_POST['name']);
+                $email = htmlspecialchars($_POST['email']);
+                $suggestion = htmlspecialchars($_POST['suggestion']);
+                $stmt = $conn->prepare("INSERT INTO suggestions (name, email, suggestion) VALUES (?, ?, ?)");
+                $stmt->bind_param(
+                    "sss",
+                    $firstName,
+                    $email,
+                    $suggestion
+                );
+                
+                
+                    $stmt->execute();
+                    
+                    $conn->close();
+                }
+                $URI = $_SERVER['REQUEST_URI'];
 
-                $conn->close();
-            }
+                    if(!empty($_POST)){
+           
+
+                    header("location:$URI");
+                    }
+                    
             ?>
 
-            <form method="post">
+            <form method="post" action="">
                 <label for="name">name (optional)</label>
                 <input type="text" name="name" class="mt-3"><br>
                 <label for="email">email (optional)</label>
                 <input type="email" name="email" class="mt-3"> <br>
                 <label for="suggestion" class="mt-3">Suggestion: </label> <br>
-                <textarea name="suggestion" id="suggestion" cols="50" rows="10" placeholder="Enter your suggestion...">
+                <textarea name="suggestion" id="suggestion" cols="50" rows="10" placeholder="Enter your suggestion..." class="mt-3">
 
                     </textarea>
                 <br>
-                <button class="btn btn-success" type="submit" name="submit">Submit</button>
+                <button class="btn btn-success mt-3" type="submit" name="submit">Submit</button>
         </div>
         </form>
     </main>
