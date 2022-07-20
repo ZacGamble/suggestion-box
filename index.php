dc3bde16a78b7d44415e45c824d74aff45d73f6a
gagelasher <rsgage21@gmail.com>
2022-07-20 11:43:02 -0600

    cleaning up

--- index.php
+++ index.php
@@ -1,48 +1,14 @@
-
 <?php
 require('header.php');
 require('connection.php');
-
 ?>
-
 <body>
     <header class="d-flex align-items-center justify-content-center">
         <img src="https://images.unsplash.com/photo-1516414447565-b14be0adf13e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFuJTIwd3JpdGluZyUyMG5vdGV8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" class="header-img">
     </header>
     <main>
-        <div class="p-5 d-flex align-items-center justify-content-center">
-
-            <?php
-
-            if (isset($_POST['submit'])) {
-                $firstName = htmlspecialchars($_POST['name']);
-                $email = htmlspecialchars($_POST['email']);
-                $suggestion = htmlspecialchars($_POST['suggestion']);
-                $stmt = $conn->prepare("INSERT INTO suggestions (name, email, suggestion) VALUES (?, ?, ?)");
-                $stmt->bind_param(
-                    "sss",
-                    $firstName,
-                    $email,
-                    $suggestion
-                );
-                
-                
-                    $stmt->execute();
-                    
-                    $conn->close();
-                }
-                $URI = $_SERVER['REQUEST_URI'];
-
-                    if(!empty($_POST)){
-           
-
-                    header("location:$URI");
-                    }
-                    
-            ?>
-
-            <form method="post" action="">
-                
+        <div class="p-5 d-flex align-items-center justify-content-center">  
+            <form method="post" action="submit.php">
                 <input type="text" name="name" class="mt-3" placeholder="Name.."><br>
                 <input type="email" name="email" class="mt-3" placeholder="Email..."> <br>
                 <textarea name="suggestion" id="suggestion" cols="50" rows="10" placeholder="Enter your suggestion..." class="mt-3"></textarea>
@@ -51,7 +17,5 @@ require('connection.php');
         </div>
         </form>
     </main>
-
 </body>
-
 </html>
